6c8b4599ea9417f4d33518700110a8f0fb181e89
miguel <vackap@gmail.com>
2025-03-04 21:23:39 -0300

    working interrupts

--- src/main.cpp
+++ src/main.cpp
@@ -6,34 +6,56 @@ bool done = false;
 uint64_t count = 0;
 uint8_t blink_pin = PB0;
 
-ISR(INT0_vect) {
-    PORTB |= (1 << PB0);
+ISR(PCINT0_vect) {
+    // disable interrupts
+    GIMSK &= ~(1 << PCIE);
+
+    PORTB |= (1 << PB4);
     sleep(WDTO_60MS);
-    PORTB &= ~(1 << PB0);
+    PORTB &= ~(1 << PB4);
     sleep();
+    sleep();
+
+    // re-enable interrupts
+    GIMSK |= (1 << PCIE);
+
 }
 
 void setup_interrupt() {
-    // The External Interrupt Control Register A 
+    // The External Interrupt Control Register A: ref page 51
     // contains control bits for interrupt sense control.
+
     // The rising edge of INT0 generates an interrupt request.
-    MCUCR |= (1 << ISC01) | (1 << ISC00);
+    // MCUCR |= (1 << ISC01) | (1 << ISC00);
+
+    // The falling edge of INT0 generates an interrupt request.
+    MCUCR |= (0 << ISC01) | (1 << ISC00);
 
     // General Interrupt Mask Register
-    // enabling external interrupt flag
-    GIMSK |= (1 << INT0);
 
-    PCMSK |= (1 << PB1);
+    // enabling external interrupt flag
+    // GIMSK |= (1 << INT0);
+
+    // enabling pin change interrupt flag
+    GIMSK |= (1 << PCIE);
+
+    // enable mask on PB3
+    PCMSK |= (1 << PB3);
 }
 
 void setup() {
 
     pins_as_input();
-    
-    // set pints as output
-    DDRB |= (1 << DDB0) | (1 << DDB4)| (1 << DDB3);
+
+    // set pins
+    // 0 = input
+    // 1 = output
+    DDRB |= (1 << DDB0) | (1 << DDB4);
+    DDRB &= ~(1 << DDB3);
 
     PORTB = 0;
+    // if input, 1 = pullup
+    PORTB |= (1 << PORTB3);
 
     setup_interrupt();
 
@@ -56,11 +78,5 @@ void loop() {
         blink(blink_pin);
 
         count += 2;
-
-        if (count > 20) {
-            blink_pin = PB3;
-        } else if (count > 10) {
-            blink_pin = PB4;
-        }
     }
 }
